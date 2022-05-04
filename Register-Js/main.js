c5c1342cebfcc5ba15279fc68dd0839bc81c2e4f
Ghu1iyev <tu12n4c1r@code.edu.az>
2022-05-04 11:29:22 +0400

    task0js

---
+++ Register-Js/main.js
@@ -0,0 +1,17 @@
+var username = prompt('Please Enter Username :');
+var password = prompt('Please Enter Password :');
+
+if (username == 'username1' && password == 'password1') {
+    console.log('Correct');
+}else{
+    console.log('Incorrect');
+}
+
+
+// var result = prompt('Please Enter Username');
+// var result2 = prompt('Please Enter password')
+// if (result == 'username1' && result2 == 'password1') {
+//     console.log('correct');
+// }else{
+//     console.log('yanlisdir');
+// }
